7da4e6535f96dd67b85336813f15d6b30041b1ed
Isaac <Isaacperron@hotmail.com>
2020-03-24 07:24:50 -0400

    Implemented MonoBehaviour Extension RequireComponent

---
+++ Scripts/MonoBehaviourExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Isaac.Tools
+{
+    public static class MonoBehaviourExtensions
+    {
+
+        public static T RequireComponent<T>(this MonoBehaviour behaviour) where T : UnityEngine.Component
+        {
+            T component = behaviour.GetComponent<T>();
+            if(component == null) return behaviour.gameObject.AddComponent<T>();
+            return component;
+        }
+
+    }
+}
