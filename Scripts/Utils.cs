f51234fa57f8da58db919da07fb5f71b81410990
Isaac <IsaacPerron@hotmail.com>
2021-12-14 07:49:36 -0500

    Implemented Tilemap and Rect line functions
    -Elanetic.Tools asmdef file now has allow unsafe code set to true.
    -Renamed Utils.Normalized to NormalizedDirection.
    -Implemented GridLineOverlap that gets all tile coordinates across a line.
    -Implemented Utils.LineClip that outputs a line that is within a rect.
    -Implemented Utils.RaycastRect that builds off LineClip theory to get the intersection point of a ray to a rect.

--- Scripts/Utils.cs
+++ Scripts/Utils.cs
@@ -1,9 +1,11 @@
 ﻿///
-using UnityEngine;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+
+using UnityEngine;
+using UnityEngine.Tilemaps;
 
 using Random = UnityEngine.Random;
 
@@ -126,7 +128,8 @@ namespace Elanetic.Tools
             return ((numberToAlign + (multipleOf - 1L)) / multipleOf) * multipleOf;
         }
 
-        static public Vector2 Normalized(Vector2 start, Vector2 end)
+        //Get the normalized direction from a start point towards an end point
+        static public Vector2 NormalizedDirection(Vector2 start, Vector2 end)
         {
             Vector2 heading = end - start;
             return heading / heading.magnitude;
@@ -275,6 +278,323 @@ namespace Elanetic.Tools
             }
 
             return resultTextures;
+        }
+
+        static public void GridLineOverlap(Vector2 worldStartPoint, Vector2 worldEndPoint, Grid grid, List<Vector2Int> results)
+        {
+            worldStartPoint = grid.WorldToLocal(worldStartPoint);
+            worldEndPoint = grid.WorldToLocal(worldEndPoint);
+
+            GridLineOverlap(worldStartPoint, worldEndPoint, results);
+        }
+
+        /// <summary>
+        /// Get all tile coordinates(assuming the size of the tile is 1.0f with no spacing in world space) that a line from start to end points overlaps.
+        /// </summary>
+        //Source: http://playtechs.blogspot.com/2007/03/raytracing-on-grid.html
+        static public void GridLineOverlap(Vector2 localStartPoint, Vector2 localEndPoint, List<Vector2Int> results)
+        {
+            Vector2 deltaPoint = new Vector2(Mathf.Abs(localEndPoint.x - localStartPoint.x), Mathf.Abs(localEndPoint.y - localStartPoint.y));
+
+            int posX = Mathf.FloorToInt(localStartPoint.x);
+            int posY = Mathf.FloorToInt(localStartPoint.y);
+
+            int n = 1;
+            int x_Inc, y_Inc;
+            float error;
+
+            if(deltaPoint.x == 0)
+            {
+                x_Inc = 0;
+                error = float.PositiveInfinity;
+            }
+            else if(localEndPoint.x > localStartPoint.x)
+            {
+                x_Inc = 1;
+                n += Mathf.FloorToInt(localEndPoint.x) - posX;
+                error = (Mathf.Floor(localStartPoint.x) + 1 - localStartPoint.x) * deltaPoint.y;
+            }
+            else
+            {
+                x_Inc = -1;
+                n += posX - Mathf.FloorToInt(localEndPoint.x);
+                error = (localStartPoint.x - Mathf.Floor(localStartPoint.x)) * deltaPoint.y;
+            }
+
+            if(deltaPoint.y == 0)
+            {
+                y_Inc = 0;
+                error -= float.PositiveInfinity;
+            }
+            else if(localEndPoint.y > localStartPoint.y)
+            {
+                y_Inc = 1;
+                n += Mathf.FloorToInt(localEndPoint.y) - posY;
+                error -= (Mathf.Floor(localStartPoint.y) + 1 - localStartPoint.y) * deltaPoint.x;
+            }
+            else
+            {
+                y_Inc = -1;
+                n += posY - Mathf.FloorToInt(localEndPoint.y);
+                error -= (localStartPoint.y - Mathf.Floor(localStartPoint.y)) * deltaPoint.x;
+            }
+
+            results.Clear();
+            if(results.Capacity < n)
+            {
+                //An extra 10 just for fun
+                results.Capacity = n + 10;
+            }
+
+            for(; n > 0; --n)
+            {
+                results.Add(new Vector2Int(posX, posY));
+
+                if(error > 0)
+                {
+                    posY += y_Inc;
+                    error -= deltaPoint.x;
+                }
+                else
+                {
+                    posX += x_Inc;
+                    error += deltaPoint.y;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Will be deleted due to being slower than other implementations. Use LineClip instead.
+        /// </summary>
+        //Source: https://en.wikipedia.org/wiki/Cohen%E2%80%93Sutherland_algorithm
+        /*
+        static public bool LineClipCohenSutherland(Vector2 startPoint, Vector2 endPoint, Rect rect, out ValueTuple<Vector2,Vector2> result)
+        {
+            const int INSIDE = 0; // 0000
+            const int LEFT = 1;   // 0001
+            const int RIGHT = 2;  // 0010
+            const int BOTTOM = 4; // 0100
+            const int TOP = 8;    // 1000
+
+            Vector2 min = rect.min;
+            Vector2 max = rect.max;
+
+            int ComputeOutCode(Vector2 point)
+            {
+                int code = INSIDE; // initialised as being inside of [[clip window]]
+
+                if(point.x < min.x)          // to the left of clip window
+                    code |= LEFT;
+                else if(point.x > max.x)      // to the right of clip window
+                    code |= RIGHT;
+                if(point.y < min.y)           // below the clip window
+                    code |= BOTTOM;
+                else if(point.y > max.y)      // above the clip window
+                    code |= TOP;
+
+                return code;
+            }
+
+            // compute outcodes for P0, P1, and whatever point lies outside the clip rectangle
+            int outCode0 = ComputeOutCode(startPoint);
+            int outCode1 = ComputeOutCode(endPoint);
+
+
+            bool accept = false;
+
+            while(true)
+            {
+                if((outCode0 | outCode1) == 0)
+                {
+                    // bitwise OR is 0: both points inside window; trivially accept and exit loop
+                    accept = true;
+                    break;
+                }
+                else if((outCode0 & outCode1) != 0)
+                {
+                    // bitwise AND is not 0: both points share an outside zone (LEFT, RIGHT, TOP,
+                    // or BOTTOM), so both must be outside window; exit loop (accept is false)
+                    break;
+                }
+                else
+                {
+                    // failed both tests, so calculate the line segment to clip
+                    // from an outside point to an intersection with clip edge
+                    float x, y;
+
+                    int outcodeOut = outCode1 > outCode0 ? outCode1 : outCode0;
+
+                    // Now find the intersection point;
+                    // use formulas:
+                    //   slope = (y1 - y0) / (x1 - x0)
+                    //   x = x0 + (1 / slope) * (ym - y0), where ym is ymin or ymax
+                    //   y = y0 + slope * (xm - x0), where xm is xmin or xmax
+                    // No need to worry about divide-by-zero because, in each case, the
+                    // outcode bit being tested guarantees the denominator is non-zero
+                    if((outcodeOut & TOP) != 0)
+                    {
+                        x = startPoint.x + (endPoint.x - startPoint.x) * (max.y - startPoint.y) / (endPoint.y - startPoint.y);
+                        y = max.y;
+                    }
+                    else if((outcodeOut & BOTTOM) != 0)
+                    {
+                        // point is below the clip window
+                        x = startPoint.x + (endPoint.x - startPoint.x) * (min.y - startPoint.y) / (endPoint.y - startPoint.y);
+                        y = min.y;
+                    }
+                    else if((outcodeOut & RIGHT) != 0)
+                    {
+                        // point is to the right of clip window
+                        y = startPoint.y + (endPoint.y - startPoint.y) * (max.x - startPoint.x) / (endPoint.x - startPoint.x);
+                        x = max.x;
+                    }
+                    else if((outcodeOut & LEFT) != 0)
+                    {
+                        // point is to the left of clip window
+                        y = startPoint.y + (endPoint.y - startPoint.y) * (min.x - startPoint.x) / (endPoint.x - startPoint.x);
+                        x = min.x;
+                    }
+                    else
+                    {
+                        x = float.NaN;
+                        y = float.NaN;
+                    }
+
+                    // Now we move outside point to intersection point to clip
+                    // and get ready for next pass.
+                    if(outcodeOut == outCode0)
+                    {
+                        startPoint = new Vector2(x, y);
+                        outCode0 = ComputeOutCode(startPoint);
+                    }
+                    else
+                    {
+                        endPoint = new Vector2(x, y);
+                        outCode1 = ComputeOutCode(endPoint);
+                    }
+                }
+            }
+
+            result = new ValueTuple<Vector2, Vector2>(startPoint, endPoint);
+            return accept;
+        }
+        */
+
+        /// <summary>
+        /// Cast a line with a rect and trim the line to be contained within the rect. Returns a bool with whether the line overlaps the rect or not. Output variable is the resulting line that is contained within the rect.
+        /// A modified version of the proposed algorithm from https://arxiv.org/abs/1908.01350.
+        /// I've verified this proposed white paper(or at least my implementation) is faster than the Cohen-Sutherland algorithm on Wikipedia(https://en.wikipedia.org/wiki/Cohen%E2%80%93Sutherland_algorithm).
+        /// Differences is that there is no loop or temporary array since I believe an array lookup would be slower(even using a stackalloc pointer or span for a stack allocated array) than a direct access to a local variable and the loop is definitely not needed(and potentially slower but not 100%).
+        /// </summary>
+        static public bool LineClip(Vector2 startPoint, Vector2 endPoint, Rect rect, out ValueTuple<Vector2, Vector2> result)
+        {
+            Vector2 min = rect.min;
+            Vector2 max = rect.max;
+            
+            if(!(startPoint.x < min.x && endPoint.x < min.x)  && !(startPoint.x > max.x && endPoint.x > max.x))
+            {
+                if(!(startPoint.y < min.y && endPoint.y < min.y) && !(startPoint.y > max.y && endPoint.y < max.y))
+                {
+                    Vector2 resultStart = startPoint;
+                    Vector2 resultEnd = endPoint;
+
+                    //Determine result start
+                    if(resultStart.x < min.x)
+                    {
+                        resultStart = new Vector2(min.x, ((endPoint.y - startPoint.y) / (endPoint.x - startPoint.x)) * (min.x - startPoint.x) + startPoint.y);
+                    }
+                    else if(resultStart.x > max.x)
+                    {
+                        resultStart = new Vector2(max.x, ((endPoint.y - startPoint.y) / (endPoint.x - startPoint.x)) * (max.x - startPoint.x) + startPoint.y);
+                    }
+
+                    if(resultStart.y < min.y)
+                    {
+                        resultStart = new Vector2(((endPoint.x - startPoint.x) / (endPoint.y - startPoint.y)) * (min.y - startPoint.y) + startPoint.x, min.y);
+                    }
+                    else if(resultStart.y > max.y)
+                    {
+                        resultStart = new Vector2(((endPoint.x - startPoint.x) / (endPoint.y - startPoint.y)) * (max.y - startPoint.y) + startPoint.x, max.y);
+                    }
+
+                    //Determine result end
+                    if(resultEnd.x < min.x)
+                    {
+                        resultEnd = new Vector2(min.x, ((endPoint.y - startPoint.y) / (endPoint.x - startPoint.x)) * (min.x - startPoint.x) + startPoint.y);
+                    }
+                    else if(resultEnd.x > max.x)
+                    {
+                        resultEnd = new Vector2(max.x, ((endPoint.y - startPoint.y) / (endPoint.x - startPoint.x)) * (max.x - startPoint.x) + startPoint.y);
+                    }
+
+                    if(resultEnd.y < min.y)
+                    {
+                        resultEnd = new Vector2(((endPoint.x - startPoint.x) / (endPoint.y - startPoint.y)) * (min.y - startPoint.y) + startPoint.x, min.y);
+                    }
+                    else if(resultEnd.y > max.y)
+                    {
+                        resultEnd = new Vector2(((endPoint.x - startPoint.x) / (endPoint.y - startPoint.y)) * (max.y - startPoint.y) + startPoint.x, max.y);
+                    }
+
+                    if(!(resultStart.x < min.x && resultEnd.x < min.x) && !(resultStart.x > max.x && resultEnd.x > max.x))
+                    {
+                        result = new ValueTuple<Vector2, Vector2>(resultStart, resultEnd);
+                        return true;
+                    }
+                }
+            }
+
+            //The line does not overlap with the rect
+            result = new ValueTuple<Vector2, Vector2>(startPoint, endPoint);
+            return false;
+        }
+
+        /// <summary>
+        /// Cast a ray towards a rect. Returns true if the ray intersects with rect. The hitpoint is outputted as a result. 
+        /// </summary>
+        static public bool RaycastRect(Ray2D ray, Rect rect, out Vector2 hitPoint)
+        {
+            Vector2 min = rect.min;
+            Vector2 max = rect.max;
+
+            Vector2 endPoint = ray.direction * float.MaxValue;
+
+            if(!(ray.origin.x < min.x && endPoint.x < min.x) && !(ray.origin.x > max.x && endPoint.x > max.x))
+            {
+                if(!(ray.origin.y < min.y && endPoint.y < min.y) && !(ray.origin.y > max.y && endPoint.y < max.y))
+                {
+                    Vector2 resultStart = ray.origin;
+
+                    //Determine result start
+                    if(resultStart.x < min.x)
+                    {
+                        resultStart = new Vector2(min.x, ((endPoint.y - ray.origin.y) / (endPoint.x - ray.origin.x)) * (min.x - ray.origin.x) + ray.origin.y);
+                    }
+                    else if(resultStart.x > max.x)
+                    {
+                        resultStart = new Vector2(max.x, ((endPoint.y - ray.origin.y) / (endPoint.x - ray.origin.x)) * (max.x - ray.origin.x) + ray.origin.y);
+                    }
+
+                    if(resultStart.y < min.y)
+                    {
+                        resultStart = new Vector2(((endPoint.x - ray.origin.x) / (endPoint.y - ray.origin.y)) * (min.y - ray.origin.y) + ray.origin.x, min.y);
+                    }
+                    else if(resultStart.y > max.y)
+                    {
+                        resultStart = new Vector2(((endPoint.x - ray.origin.x) / (endPoint.y - ray.origin.y)) * (max.y - ray.origin.y) + ray.origin.x, max.y);
+                    }
+
+                    if(!(resultStart.x < min.x) && !(resultStart.x > max.x))
+                    {
+                        hitPoint = resultStart;
+                        return true;
+                    }
+                }
+            }
+
+            //The ray does not intersect with the rect
+            hitPoint = new Vector2(float.NaN, float.NaN);
+            return false;
         }
 
         static public void DrawBounds(BoundsInt bounds) => DrawBounds(new Bounds(bounds.center, bounds.size));
