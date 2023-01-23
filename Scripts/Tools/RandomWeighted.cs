e0eb3aa8e6aea3772cc989a7810dc447a572a909
Isaac <IsaacPerron@hotmail.com>
2023-01-23 17:56:09 -0500

    RandomWeighted Now Supports Setting Seed

--- Scripts/Tools/RandomWeighted.cs
+++ Scripts/Tools/RandomWeighted.cs
@@ -8,6 +8,11 @@ namespace Elanetic.Tools
 
 		static private FastRandom m_FastRandom;
 
+		static RandomWeighted()
+		{
+			m_FastRandom = new FastRandom();
+		}
+
         /// <summary>
         /// Get the index of a randomly chosen inputted value. Each value is weighted where bigger values will have a bigger chance of being chosen.
         /// Negative values are treated with a weight of 0. If all values have the same weight(including each having a weight of 0) they will have an equal chance of being chosen.
@@ -21,17 +26,18 @@ namespace Elanetic.Tools
 			}
 #endif
 
-			if(m_FastRandom == null) m_FastRandom = new FastRandom();
-
 			float total = 0;
 			for(int i = 0; i < weights.Length; i++)
 			{
 				float value = weights[i];
+				
+				//If the value is negative it has not weight and should not affect the total
 				if(value < 0) continue;
 				total += value;
 			}
 
-			//If all the values given are 0, make all values the same weight
+			//If all the values given are 0 or less, make all values the same weight
+			//Negative values are treated as weight of 0
 			if(total == 0.0f)
 			{
 				return m_FastRandom.Range(0, weights.Length);
@@ -67,6 +73,11 @@ namespace Elanetic.Tools
 			if(m_FastRandom.Range(0.0f, value0 + value1) <= value0)
 				return 0;
 			return 1;
+		}
+
+		static public void SetSeed(int seed)
+		{
+			m_FastRandom.SetSeed(seed);
 		}
 	}
 }
